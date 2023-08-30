456a4acd68d893d6052d9db143defe84d6a75c5d
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2023-08-30 23:49:31 +0530

    practiced Strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,35 +1,64 @@
-const name = "hitesh"
-const repoCount = 50
+const name = "hitesh";
+const repoCount = 50;
 
 // console.log(name + repoCount + " Value");
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('hitesh-hc-com')
+const gameName = new String("hitesh-hc-com");
 
 // console.log(gameName[0]);
-// console.log(gameName.__proto__);
-
+console.log(gameName.__proto__);
 
 // console.log(gameName.length);
 // console.log(gameName.toUpperCase());
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('t'));
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
 
-const newString = gameName.substring(0, 4)
-console.log(newString);
+const newString = gameName.substring(0, 4);
+// console.log(newString);
 
-const anotherString = gameName.slice(-8, 4)
-console.log(anotherString);
+const anotherString = gameName.slice(-10, 13);
+// console.log(anotherString, " : slice");
 
-const newStringOne = "   hitesh    "
-console.log(newStringOne);
-console.log(newStringOne.trim());
+const newStringOne = "   hitesh    ";
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
 
-const url = "https://hitesh.com/hitesh%20choudhary"
+const url = "https://hitesh.com/hitesh%20choudhary"; // %20 url encoding exmaple
 
-console.log(url.replace('%20', '-'))
+// console.log(url.replace("%20", "-"));
 
-console.log(url.includes('sundar'))
+// console.log(url.includes("sundar"));
 
-console.log(gameName.split('-'));
+console.log(gameName.split("-")); // "hitesh-hc-com"
+const str = "The quick brown fox jumps over the lazy dog.";
+
+const words = str.split(" ");
+console.log(words[3], str.split(" "));
+// Expected output: "fox"
+
+const chars = str.split(""); // withou limiter
+console.log(chars[8], str.split("", 14)); // with limiter
+// Expected output: "k"
+
+const strCopy = str.split();
+console.log(strCopy);
+// Expected output: Array ["The quick brown fox jumps over the lazy dog."]
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+//.........................................
+
+console.log(eval("2 + 2"));
+// Expected output: 4
+
+console.log(eval(new String("2 + 2")));
+// Expected output: 2 + 2
+
+console.log(eval("2 + 2") === eval("4"));
+// Expected output: true
+
+console.log(eval("2 + 2") === eval(new String("2 + 2")));
+// Expected output: false
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/eval
