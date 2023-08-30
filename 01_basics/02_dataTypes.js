ca976e2d2434b2322515fa77748693c538ef472a
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2023-08-30 17:13:57 +0530

    practiced data types

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -2,28 +2,25 @@
 
 // alert( 3 + 3) // we are using nodejs, not browser
 
-console.log(3 
-    +
-     3) // code readability should be high
+console.log(3 + 3); // code readability should be high
 
-console.log("Hitesh")
+console.log("Hitesh");
 
-
-let name = "hitesh"
-let age = 18
-let isLoggedIn = false
+let name = "hitesh";
+let age = 18;
+let isLoggedIn = false;
 let state;
 
-// number => 2 to power 53
+// number => 2 to power 53 : range
 // bigint
 // string => ""
 // boolean => true/false
-// null => standalone value
-// undefined => 
+// null =>   : standalone value, represents an intentional empty value.
+// undefined => : value is not assigned yet.
 // symbol => unique
-
 
 // object
 
+console.log(typeof age);
 console.log(typeof undefined); // undefined
 console.log(typeof null); // object
