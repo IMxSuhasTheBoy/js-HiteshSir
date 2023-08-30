7400d9a110fdf380db9d20c10fe3ca63c8f922eb
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2023-08-30 18:04:59 +0530

    practiced converOps

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,36 +1,40 @@
-let score = "hitesh"
+let score = "33";
+// score = "33noo"; // NaN  when converted into Number.
 
-//console.log(typeof score);
-//console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score)
-//console.log(typeof valueInNumber);
-//console.log(valueInNumber);
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
 
+// console.log(Number(null)); //0
+// console.log(typeof Number(undefined)); // number : NaN
 
 // "33" => 33
 // "33abc" => NaN
 //  true => 1; false => 0
 
-let isLoggedIn = "hitesh"
+let isLoggedIn = "hitesh";
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-//  console.log(booleanIsLoggedIn);
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
 // "hitesh" => true
 
-let someNumber = 33
+//-----------------------------------------------
 
-let stringNumber = String(someNumber)
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
 
 // *********************** Operations ***********************
 
-let value = 3
-let negValue = -value
+let value = 3;
+let negValue = -value; // -3
 // console.log(negValue);
 
 // console.log(2+2);
@@ -40,10 +44,10 @@ let negValue = -value
 // console.log(2/3);
 // console.log(2%3);
 
-let str1 = "hello"
-let str2 = " hitesh"
+let str1 = "hello";
+let str2 = " hitesh";
 
-let str3 = str1 + str2
+let str3 = str1 + str2;
 // console.log(str3);
 
 // console.log("1" + 2);
@@ -56,13 +60,22 @@ let str3 = str1 + str2
 // console.log(+true);
 // console.log(+"");
 
-let num1, num2, num3
+let num1, num2, num3;
 
-num1 = num2 = num3 = 2 + 2
+num1 = num2 = num3 = 2 + 2;
+// console.log(num1, num2, num3); // lol!
 
-let gameCounter = 100
+let gameCounter = 100;
 ++gameCounter;
 console.log(gameCounter);
 
 // link to study
 // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+// prefix postfix
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+let x2 = 3n;
+const y2 = x2++;
+// console.log(`value of x2: ${x2}, it's type is: ${typeof x2}`);
+// console.log(`value of y2: ${y2}, it's type is: ${typeof y2}`);
