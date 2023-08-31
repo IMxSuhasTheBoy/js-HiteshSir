c44253a265f37f12cb5a98fc3b7086c40c0e0e1a
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2023-08-31 17:06:48 +0530

    practiced

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -1,32 +1,39 @@
 // Dates
 
-let myDate = new Date()
+let myDate = new Date();
 // console.log(myDate.toString());
 // console.log(myDate.toDateString());
 // console.log(myDate.toLocaleString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toUTCString());
+// console.log(myDate.getTimezoneOffset());
+
 // console.log(typeof myDate);
 
-// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23)  // month 1 to 12 =  in 0 based indices
 // let myCreatedDate = new Date(2023, 0, 23, 5, 3)
 // let myCreatedDate = new Date("2023-01-14")
-let myCreatedDate = new Date("01-14-2023")
+let myCreatedDate = new Date("01-14-2023");
 // console.log(myCreatedDate.toLocaleString());
 
-let myTimeStamp = Date.now()
+let myTimeStamp = Date.now();
 
 // console.log(myTimeStamp);
 // console.log(myCreatedDate.getTime());
 // console.log(Math.floor(Date.now()/1000));
 
-let newDate = new Date()
-console.log(newDate);
-console.log(newDate.getMonth() + 1);
-console.log(newDate.getDay());
+let newDate = new Date();
+// console.log(newDate);
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay()); // weekday sequence from sunday in 0 based indexing
 
 // `${newDate.getDay()} and the time `
 
-newDate.toLocaleString('default', {
-    weekday: "long",
-    
-})
-
+newDate.toLocaleString("default", {
+  weekday: "long",
+  month: "2-digit"
+});
