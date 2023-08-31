bdf465124fc48cfee24b030edbf26c67b337da2f
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2023-08-31 18:08:50 +0530

    practiced

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,28 +1,36 @@
-const marvel_heros = ["thor", "Ironman", "spiderman"]
-const dc_heros = ["superman", "flash", "batman"]
+const marvel_heros = ["thor", "Ironman", "spiderman"];
+const dc_heros = ["superman", "flash", "batman"];
 
-// marvel_heros.push(dc_heros)
+// marvel_heros.push(dc_heros)  // original arr modified
 
 // console.log(marvel_heros);
+// console.log(marvel_heros[3])
 // console.log(marvel_heros[3][1]);
 
-// const allHeros = marvel_heros.concat(dc_heros)
+// const allHeros = marvel_heros.concat(dc_heros)  // ret new arr without modifing original arr
 // console.log(allHeros);
 
-const all_new_heros = [...marvel_heros, ...dc_heros]
+// spread / rest---------------
 
-// console.log(all_new_heros);
+const all_new_heros = [...marvel_heros, ...dc_heros]; // advantage over concat method : multiple arr spread for returning into one arr
 
-const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+// console.log( all_new_heros);
 
-const real_another_array = another_array.flat(Infinity)
-console.log(real_another_array);
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
 
+const real_another_array = another_array.flat(Infinity);
+// console.log(real_another_array);
 
+// console.log(Array.isArray("Hitesh"));
+// console.log(Array.from("Hiteshfddss")); // string is converted in to arr with each indices
+console.log(Array.from({ name: "hitesh" }).values); // interesting  : to make an arr from obj must specify for key / value to have in arr, syntax to be learnt
 
-console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+//---------------------
+const set = new Set(["foo", "bar", "baz", "foo"]);
+// console.log(Array.from(set));
+// console.log(Array.of(set));
+//---------------------
+
 
 let score1 = 100
 let score2 = 200
