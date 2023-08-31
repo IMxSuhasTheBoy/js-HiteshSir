aea2d7a086012306e4d10bed91d90754c782e430
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2023-08-31 18:44:31 +0530

    practiced

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,5 +1,7 @@
-// singleton
-// Object.create
+// singleton :  made from constructor only
+// Object.create : constructor
+
+//------------------------
 
 // object literals
 
@@ -9,7 +11,8 @@ const mySym = Symbol("key1")
 const JsUser = {
     name: "Hitesh",
     "full name": "Hitesh Choudhary",
-    [mySym]: "mykey1",
+    myS: "mykey1- im normal",
+    [mySym]: "mykey2 im ref. from symbol", 
     age: 18,
     location: "Jaipur",
     email: "hitesh@google.com",
@@ -17,19 +20,23 @@ const JsUser = {
     lastLoginDays: ["Monday", "Saturday"]
 }
 
-// console.log(JsUser.email)
-// console.log(JsUser["email"])
-// console.log(JsUser["full name"])
-// console.log(JsUser[mySym])
+// console.log(JsUser.email) // dotNotation not preferd way to access, won't work with key's of other data type. 
+// console.log(JsUser["email"]) // betterr way to access, work's with key's of any data type
+// console.log(JsUser["full name"]) //  key of string type 
+// console.log(JsUser["myS"]) // normal string type key
+// console.log(JsUser[mySym])  // symbol syntax for use as key 
 
 JsUser.email = "hitesh@chatgpt.com"
-// Object.freeze(JsUser)
-JsUser.email = "hitesh@microsoft.com"
+// Object.freeze(JsUser)  // changes to obj are locked 
+JsUser.email = "hitesh@microsoft.com"  // won't give arr for trying to manipulate 
 // console.log(JsUser);
 
 JsUser.greeting = function(){
     console.log("Hello JS user");
 }
+
+// console.log(JsUser)
+
 JsUser.greetingTwo = function(){
     console.log(`Hello JS user, ${this.name}`);
 }
