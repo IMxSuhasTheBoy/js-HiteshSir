98bef3e53f55ed48951c3dc8731f3ed6a52cc15c
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2023-08-31 17:35:11 +0530

    practiced

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -4,24 +4,32 @@ const myArr = [0, 1, 2, 3, 4, 5]
 const myHeors = ["shaktiman", "naagraj"]
 
 const myArr2 = new Array(1, 2, 3, 4)
+
+console.log(myArr2)
 // console.log(myArr[1]);
 
-// Array methods
+// -----------Array methods------------
 
-// myArr.push(6)
-// myArr.push(7)
-// myArr.pop()
+myArr2.push(6)
+console.log(myArr2)
+myArr2.push(7)
+console.log(myArr2)
+myArr2.pop()
+console.log(myArr2)
+myArr2.unshift(9)   // not optimal way
+console.log(myArr2)
+myArr2.shift()
+console.log(myArr2)
 
-// myArr.unshift(9)
-// myArr.shift()
+// console.log(myArr2.includes(9));
 
-// console.log(myArr.includes(9));
-// console.log(myArr.indexOf(3));
+// console.log(myArr2.indexOf(4));
+// console.log(myArr2.indexOf(7));
 
-// const newArr = myArr.join()
+// const newArr = myArr2.join()
 
-// console.log(myArr);
-// console.log( newArr);
+// console.log(myArr2);
+// console.log( `${newArr} .join converted an into ${typeof newArr}`);
 
 
 // slice, splice
@@ -29,11 +37,12 @@ const myArr2 = new Array(1, 2, 3, 4)
 console.log("A ", myArr);
 
 const myn1 = myArr.slice(1, 3)
-
 console.log(myn1);
+
 console.log("B ", myArr);
 
+//------------------------
 
-const myn2 = myArr.splice(1, 3)
-console.log("C ", myArr);
-console.log(myn2);
+const myn2 = myArr.splice(1, 3)  // returns sub array including del count, changes original array (ref.)
+console.log("C ", myArr); // original array with leftover ele
+console.log(myn2); // got sub string 
