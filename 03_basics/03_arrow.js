8c9103c0d03bb292462cb9effdf39c3f4a6c87a2
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2023-09-01 15:08:55 +0530

    practiced

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -3,8 +3,8 @@ const user = {
     price: 999,
 
     welcomeMessage: function() {
-        console.log(`${this.username} , welcome to website`);
-        console.log(this);
+        console.log(`${this.username} , welcome to website`);  // refering curr context, accessing key of its parent scope 
+      //  console.log(this);
     }
 
 }
@@ -13,14 +13,19 @@ const user = {
 // user.username = "sam"
 // user.welcomeMessage()
 
-// console.log(this);
+// console.log(this);        // in node env. global obj -> empty obj | in browser global obj is window
 
-// function chai(){
+// function chai1(){
 //     let username = "hitesh"
-//     console.log(this.username);
+//     console.log(this.username); // not refering 
 // }
 
-// chai()
+// chai1()
+
+function kak() {
+    console.log(this)
+}
+// kak()
 
 // const chai = function () {
 //     let username = "hitesh"
@@ -31,22 +36,20 @@ const chai =  () => {
     let username = "hitesh"
     console.log(this);
 }
-
-
 // chai()
 
 // const addTwo = (num1, num2) => {
 //     return num1 + num2
-// }
+// }                                   // explicit ret
 
-// const addTwo = (num1, num2) =>  num1 + num2
+// const addTwo = (num1, num2) =>  num1 + num2    // implicit ret
 
-// const addTwo = (num1, num2) => ( num1 + num2 )
+// const addTwo = (num1, num2) => ( num1 + num2 )   // react syntax usecase
 
-const addTwo = (num1, num2) => ({username: "hitesh"})
+// const addTwo = (num1, num2) => ({username: "hitesh"})  //  -->( returning obj  )<--
 
 
-console.log(addTwo(3, 4))
+// console.log(addTwo(3, 4))
 
 
 // const myArray = [2, 5, 3, 7, 8]
