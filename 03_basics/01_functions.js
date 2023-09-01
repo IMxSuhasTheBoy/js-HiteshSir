bb185b829f41c7d208bc35aca501352521d805c3
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2023-09-01 13:01:42 +0530

    practiced

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -7,19 +7,21 @@ function sayMyName(){
     console.log("S");
     console.log("H");
 }
-
-// sayMyName()
+// sayMyName ---  reference 
+// sayMyName()  // ---  exicution
 
 // function addTwoNumbers(number1, number2){
-
-//     console.log(number1 + number2);
+//     console.log(`${number1 + number2}`);
 // }
+
+// addTwoNumbers(2, 4)
 
 function addTwoNumbers(number1, number2){
 
     // let result = number1 + number2
     // return result
-    return number1 + number2
+    return number1 + number2 
+    // Unreachable code
 }
 
 const result = addTwoNumbers(3, 5)
@@ -27,18 +29,21 @@ const result = addTwoNumbers(3, 5)
 // console.log("Result: ", result);
 
 
-function loginUserMessage(username = "sam"){
-    if(!username){
+function loginUserMessage(username = "sam"){ // if parameter have value default para will override
+    // when username is undefined, below if block conditon -> true , but we have defaul para value so, username never gona bee empty in the func block for use
+    if(!username){             
         console.log("PLease enter a username");
-        return
+        return  // just for exit the func when if block true
     }
     return `${username} just logged in`
 }
 
-// console.log(loginUserMessage("hitesh"))
-// console.log(loginUserMessage("hitesh"))
+// console.log(loginUserMessage()) // nothing as arg - so its undefined 
+// console.log(loginUserMessage("hitesh")) 
 
 
+//------shopping cart ex. indefinite values in para----
+//                                      ↓rest operator used
 function calculateCartPrice(val1, val2, ...num1){
     return num1
 }
@@ -55,10 +60,11 @@ function handleObject(anyobject){
 }
 
 // handleObject(user)
+
 handleObject({
     username: "sam",
     price: 399
-})
+})  // obj as arg
 
 const myNewArray = [200, 400, 100, 600]
 
@@ -67,4 +73,8 @@ function returnSecondValue(getArray){
 }
 
 // console.log(returnSecondValue(myNewArray));
-console.log(returnSecondValue([200, 400, 500, 1000]));
+console.log(returnSecondValue([200, 800, 500, 1000]));  // arr as arg
+
+
+
+// ret, rest, default para, obj/arr as arg 
