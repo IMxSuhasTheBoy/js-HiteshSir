e05fe090568ecb2ed0518a5ef19b641875e64bdc
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2023-09-01 17:35:29 +0530

    practiced

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -6,21 +6,21 @@ const myObject = {
 }
 
 for (const key in myObject) {
-    //console.log(`${key} shortcut is for ${myObject[key]}`);
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
 }
 
 const programming = ["js", "rb", "py", "java", "cpp"]
 
 for (const key in programming) {
-    //console.log(programming[key]);
+    // console.log(`${programming[key]}  index: ${key}`);
 }
 
-// const map = new Map()
-// map.set('IN', "India")
-// map.set('USA', "United States of America")
-// map.set('Fr', "France")
-// map.set('IN', "India")
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
 
-// for (const key in map) {
-//     console.log(key);
-// }
+for (const key of map) {
+    console.log(key);
+}                        // not iterable like arr with forin
--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -3,38 +3,39 @@
 // ["", "", ""]
 // [{}, {}, {}]
 
-const arr = [1, 2, 3, 4, 5]
+// for (const iterator of object) {}
+
+const arr = [21, 42, 13, 54, 52];
 
 for (const num of arr) {
-    //console.log(num);
+  // console.log(num);
 }
 
-const greetings = "Hello world!"
+const greetings = "Hello world!";
 for (const greet of greetings) {
-    //console.log(`Each char is ${greet}`)
+  // console.log(`Each char is ${greet}`)
 }
 
-// Maps
+// // Maps
 
-const map = new Map()
-map.set('IN', "India")
-map.set('USA', "United States of America")
-map.set('Fr', "France")
-map.set('IN', "India")
+const map1 = new Map();
+map1.set("IN", "India");
+map1.set("USA", "United States of America");
+map1.set("Fr", "France");
+map1.set("IN", "India");
 
+// console.log( map1); // duplicate entries not taken
 
-// console.log(map);
-
-for (const [key, value] of map) {
-    // console.log(key, ':-', value);
+for (const [key, value] of map1) {
+  // console.log(key, ':-', value);
 }
 
 const myObject = {
-    game1: 'NFS',
-    game2: 'Spiderman'
-}
+  game1: "NFS",
+  game2: "Spiderman",
+};
 
 // for (const [key, value] of myObject) {
-//     console.log(key, ':-', value);
-    
-// }
+//      console.log(key, ':-', value);
+
+// }       // err , cause normal obj are not iterable this way
