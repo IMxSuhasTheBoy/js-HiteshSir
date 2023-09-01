f5ba9d83179756af9f7e0c89592b1c0ea39b5c01
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2023-09-01 17:47:08 +0530

    practiced

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -1,39 +1,39 @@
-const coding = ["js", "ruby", "java", "python", "cpp"]
+const coding = ["js", "ruby", "java", "python", "cpp"];
 
 // coding.forEach( function (val){
 //     console.log(val);
-// } )
+// } )                             // method: callback (normal func)
 
 // coding.forEach( (item) => {
 //     console.log(item);
-// } )
+// } )                             //  method: callback (arrow func)
 
-// function printMe(item){
-//     console.log(item);
-// }
+function printMe(item) {
+  console.log(item);
+}
 
-// coding.forEach(printMe)
+// coding.forEach(printMe)        // call a func as for execution
 
 // coding.forEach( (item, index, arr)=> {
 //     console.log(item, index, arr);
-// } )
+// } )                               // getting all possible details
 
 const myCoding = [
-    {
-        languageName: "javascript",
-        languageFileName: "js"
-    },
-    {
-        languageName: "java",
-        languageFileName: "java"
-    },
-    {
-        languageName: "python",
-        languageFileName: "py"
-    },
-]
+  {
+    languageName: "javascript",
+    languageFileName: "js",
+  },
+  {
+    languageName: "java",
+    languageFileName: "java",
+  },
+  {
+    languageName: "python",
+    languageFileName: "py",
+  },
+];
 
-myCoding.forEach( (item) => {
-    
-    console.log(item.languageName);
-} )
+myCoding.forEach((item) => {
+  // console.log(item.languageFileName , "a");
+  // console.log(item.languageName, "b")
+});
