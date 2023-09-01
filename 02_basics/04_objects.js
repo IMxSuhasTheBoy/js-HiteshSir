2500190e3c0f32aaab085e64a0b034bb466cd074
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2023-09-01 12:04:44 +0530

    practiced

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,5 +1,7 @@
-// const tinderUser = new Object()
-const tinderUser = {}
+// const tinderUser = new Object() 
+
+// makes non singleton obj
+const tinderUser = {}    
 
 tinderUser.id = "123abc"
 tinderUser.name = "Sammy"
@@ -12,7 +14,7 @@ const regularUser = {
     fullname: {
         userfullname: {
             firstname: "hitesh",
-            lastname: "choudhary"
+            lastname: "choudhary",
         }
     }
 }
@@ -23,17 +25,20 @@ const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
-// const obj3 = { obj1, obj2 }
-// const obj3 = Object.assign({}, obj1, obj2, obj4)
+// const obj3 = { obj1, obj2 } // wasted
+// console.log(obj3["obj1"]); 
 
-const obj3 = {...obj1, ...obj2}
-// console.log(obj3);
+// obj3 : targe    optional ↓,      sources        : optonal: empty  obj to acc. sources .
+// const obj3 = Object.assign({}, obj1, obj2, obj4) // works : object assign mdn
 
+const obj3 = {...obj1, ...obj2} // easier with spread
+
+// console.log(obj3)
 
 const users = [
     {
         id: 1,
-        email: "h@gmail.com"
+        email: "hasdf@gmail.com"
     },
     {
         id: 1,
@@ -45,15 +50,23 @@ const users = [
     },
 ]
 
-users[1].email
+//----------------------
+// const test = users.map((ele)=>{
+//     console.log(ele.email)
+// })
+//----------------------
+
+// console.log(users[0].email)
 // console.log(tinderUser);
 
-// console.log(Object.keys(tinderUser));
-// console.log(Object.values(tinderUser));
-// console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));  // ret. arr of key's can be usefull
+// console.log(Object.values(tinderUser)); // arr of values ,  all arr methods apllicable, when got from constructor like this
+// console.log(Object.entries(tinderUser)); // amazing
 
 // console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
+
+//-----destructuring  objects , are imp in react-----
 
 const course = {
     coursename: "js in hindi",
@@ -61,22 +74,31 @@ const course = {
     courseInstructor: "hitesh"
 }
 
-// course.courseInstructor
+// // course.courseInstructor
 
-const {courseInstructor: instructor} = course
+// const {courseInstructor} = course
+// console.log(courseInstructor);      // destructure : key is now a const with it's value
 
-// console.log(courseInstructor);
-console.log(instructor);
 
+const {courseInstructor: instructor} = course 
+const {coursename: nameOfCourse} = course      // destructure, with const name as we want 
+// console.log(`${instructor} with ${nameOfCourse}, is awesome!`);
+
+
+// ----------------json structure ex.: api data got in object kind form 
 // {
 //     "name": "hitesh",
 //     "coursename": "js in hindi",
 //     "price": "free"
 // }
+// ----------------json------------structure ex.
 
+// -------api data got in arr of obj kind form 
 [
     {},
     {},
     {}
 ]
 
+// tree form of json data is easier for understanding!
+// json js object notation : for universal format use of data through api fetch
