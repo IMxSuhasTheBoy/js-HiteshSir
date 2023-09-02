5a8845b8874d70dda3783984552febcb8c4f658a
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2023-09-02 11:24:11 +0530

    practiced

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -1,26 +1,31 @@
-// const coding = ["js", "ruby", "java", "python", "cpp"]
+const coding = ["js", "ruby", "java", "python", "cpp"]
 
 
-// const values = coding.forEach( (item) => {
-//     //console.log(item);
-//     return item
-// } )
+const values = coding.forEach( (item) => {
+    // console.log(item);
+    return item
+} )
 
 // console.log(values);
 
+
+
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
 
 // const newNums = myNums.filter( (num) => {
 //     return num > 4
 // } )
+// console.log(newNums)
 
-// const newNums = []
+const newNums = []
 
-// myNums.forEach( (num) => {
-//     if (num > 4) {
-//         newNums.push(num)
-//     }
-// } )
+myNums.forEach( (num) => {
+    if (num > 4) {
+        newNums.push(num)
+    }
+} )
 
 // console.log(newNums);
 
@@ -37,7 +42,7 @@ const books = [
     { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
   ];
 
-  let userBooks = books.filter( (bk) => bk.genre === 'History')
+  // let userBooks = books.filter( (bk) => bk.genre === 'History')
 
   userBooks = books.filter( (bk) => { 
     return bk.publish >= 1995 && bk.genre === "History"
